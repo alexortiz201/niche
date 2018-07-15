1f72bfb5b23a984597833ea1f50f2c4710b7d5de
Alex Ortiz <aortiz@shutterstock.com>
2018-07-15 13:19:11 -0400

    Got app into a working state with redux, rxjs, reacstrap, and react router.

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+
+// global css
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App/App';
+
+// Import store and history
+import store, { history } from './redux/store';
+
+import { RoutesWrapperFactory } from './routes/index.js';
+import routes from './routes/routes';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * https://stackoverflow.com/questions/34130539/uncaught-error-invariant-violation-element-type-is-invalid-expected-a-string
+ * This throws if you use v4 of 'react-router-redux'
+ */
+render(
+  <RoutesWrapperFactory
+    store={store}
+    history={history}
+    routes={routes} />,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
